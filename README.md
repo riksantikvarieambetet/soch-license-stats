ab6d4ad1d97b109cdeba9563f47a31ee33bf2f3a
Marcus Smith <marcus@runinskrifter.net>
2018-09-14 11:51:03 +0200

    Added Perl rewrite.
    Signed-off-by: Marcus Smith <marcus@runinskrifter.net>

--- README.md
+++ README.md
@@ -22,3 +22,19 @@ This program needs pipenv to be installed on your computer.
 2. Run `pipenv run python liz.py` within this directory.
 3. Enter your SOCH API key when prompted to do so.
 4. After a while a CSV file will be generated in this directory.
+
+---
+
+# `soch-license-stats.pl`
+
+`liz.py` didn't really do everything I wanted, and I don't speak Python, so I rewrote it in Perl. (This has the added side-effect that it's now even easier to read. 😉)
+
+`soch-license-stats.pl` outputs a CSV matrix showing, for each data provider, how many images they publish under each of the different licenses that are in use in SOCH, as well as images with no licensing information, and the total number of images provided. The list of providers and licenses in use are fetched at run time.
+
+## Running
+
+1. `soch-license-stats.pl` requires Perl 5.22 or greater, and uses a few non-core modules. Using [`cpanm`](https://metacpan.org/pod/App::cpanminus), run `cpanm LWP::UserAgent Text::CSV URI::Escape XML::XPath` to install them.
+1. Run `soch-license-stats.pl`
+1. Enter your SOCH API key when prompted to do so, or press RETURN to use a default test key.
+1. …time passes…
+1. A CSV appears in the current working directory!
---
+++ soch-license-stats.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use 5.022;
+use open qw(:utf8 :std);
+
+use autodie qw(:file);
+use LWP::UserAgent;
+use Text::CSV;
+use URI::Escape qw(uri_escape);
+use XML::XPath;
+
+# Enforce proper garbage collection on XML::XPath:
+$XML::XPath::SafeMode = 1;
+
+say q(Enter your API key, or press RETURN to use a test key:);
+my $key = readline();
+chomp($key);
+$key ||= 'test-h3935Q0m';
+my $base = join('', 'http://kulturarvsdata.se/ksamsok/api?x-api=', $key, '&version=1.1');
+
+my $ua = LWP::UserAgent->new();
+$ua->agent('SOCH-licenses 0.1');
+$ua->default_header('Accept' => 'application/rdf+xml, application/xml, text/xml');
+my $stats_path = '/result/term/indexFields/value/text()';
+my $count_path = '/result/totalHits/text()';
+
+open my $fh, '>', 'org-licenses.csv';
+my $csv = Text::CSV->new({binary => 1});
+
+# Query SOCH for a list of all service organisations providing data:
+my $req = HTTP::Request->new(GET => join('', $base, '&method=statistic', '&index=serviceOrganization=*'));
+my @providers = get_values($ua, $req, $stats_path);
+
+# Query SOCH for a list of all licenses in use, plus the empty string:
+$req = HTTP::Request->new(GET => join('', $base, '&method=statistic', '&index=mediaLicense=*'));
+my @licenses = (get_values($ua, $req, $stats_path), '');
+$csv->say($fh, ['Leverantör', 'Totala objekt', sort @licenses]);
+
+for my $provider (sort @providers) {
+	my $req = HTTP::Request->new(GET => join('', $base, '&method=search', '&hitsPerPage=1', '&startRecord=1', '&query=', uri_escape(join('', 'thumbnailExists=j AND serviceOrganization="')), $provider, '"'));
+	my ($total) = get_values($ua, $req, $count_path);
+	my @fields;
+	push @fields, $provider, $total;
+	for my $license (sort @licenses) {
+		my $req = HTTP::Request->new(GET => join('', $base, '&method=search', '&hitsPerPage=1', '&startRecord=1', '&query=', uri_escape(join('', 'thumbnailExists=j AND serviceOrganization="')), $provider, uri_escape(join('', '" AND mediaLicense="', $license, '"'))));
+		my ($instances) = get_values($ua, $req, $count_path);
+		push @fields, $instances;
+	}
+	$csv->say($fh, \@fields);
+}
+close $fh;
+
+# Fetch result for a given query:
+sub get_values {
+	my ($ua, $req, $path) = @_;
+	$req->accept_decodable;
+	my $response = $ua->request($req);
+	unless ($response->is_success) {
+		die 'Error executing query: ', $response->status_line, "\n";
+	}
+	my $xp = XML::XPath->new($response->decoded_content);
+	my $values = $xp->find($path);
+	my @values = map {XML::XPath::XMLParser::as_string($_)} $values->get_nodelist();
+	$xp->cleanup();
+	return @values;
+}
